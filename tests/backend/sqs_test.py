c2199c9b6334dd0f7def3fb6ed4dc6d46a00ade0
Travis Thieman <travis.thieman@gmail.com>
2015-03-06 10:40:36 -0500

    Mock out SQS in this test

--- tests/backend/sqs_test.py
+++ tests/backend/sqs_test.py
@@ -1,14 +1,20 @@
 from unittest import TestCase
 
 from boto.sqs.connection import SQSConnection
+from moto import mock_sqs
 
 from deferrable.backend.sqs import SQSBackendFactory, SQSBackend
 from deferrable.queue.sqs import SQSQueue
 
 class TestSQSBackendFactory(TestCase):
     def setUp(self):
+        self.fake_sqs = mock_sqs()
+        self.fake_sqs.start()
         self.sqs_connection = SQSConnection()
-        self.factory = SQSBackendFactory(self.sqs_connection)
+        self.factory = SQSBackendFactory(self.sqs_connection, create_if_missing=True)
+
+    def tearDown(self):
+        self.fake_sqs.stop()
 
     def test_create_backend_for_group(self):
         for group in ['testing']:
