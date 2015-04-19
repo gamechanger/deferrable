8b127a3d934470aa20fbff83d06ded2e37d00579
Travis Thieman <travis.thieman@gmail.com>
2015-04-18 20:40:21 -0400

    Add back some reasoning on the 900 number

--- deferrable/delay.py
+++ deferrable/delay.py
@@ -2,4 +2,7 @@
 separate this out so that deferrable.py and its sub-modules
 could all import it without circular imports."""
 
+# SQS has a hard limit of 900 seconds, and Dockets
+# delay queues incur heavy performance penalties,
+# so this seems like a reasonable limit for all
 MAXIMUM_DELAY_SECONDS = 900
